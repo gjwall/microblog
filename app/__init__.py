4bd9965411fb8c3cd82526cc57e90b2dc0f81296
gjwall <gjwall@users.noreply.github.com>
2018-03-11 20:07:27 +0000

    After tutorial 3 of blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+#
+# To execute the app, go to the command line and top level directory:
+#
+# set FLASK_APP=microblog.py   (or export for UNIX)
+# flask run
+#
+#
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
